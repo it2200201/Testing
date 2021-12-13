eeb3e28dacc46068a65bc945a429f1a5c8aa0036
ZIRVIN KIRK VILLADUZ <2200201@slu.edu.ph>
2021-12-14 01:00:00 +0800

    Initial commit Main.swift

---
+++ main.swift
@@ -0,0 +1,11 @@
+func evenOrOdd(_ number:Int) -> String {
+    if number % 2 == 0 {
+        return "Even"
+    } else {
+        return "Odd"
+    }
+}
+
+print("Input integer: ")
+let integer = Int(readLine()!)
+print(evenOrOdd(Int(integer!)))
